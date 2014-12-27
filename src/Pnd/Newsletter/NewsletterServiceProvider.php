f4760d4aebda32c0030a5afbd440509dc305a311
Pratik <prateeik.sharma@gmail.com>
2014-12-27 15:40:23 +0545

    initial commit

---
+++ src/Pnd/Newsletter/NewsletterServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Pnd\Newsletter;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewsletterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('pnd/newsletter');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
